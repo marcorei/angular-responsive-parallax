ff7d44aca78b758349a173026aa91ca0dfcaacd9
Markus Riegel <riegel.markus@googlemail.com>
2015-11-15 18:32:47 +0100

    Add gulp for min version

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var rename = require("gulp-rename");
+
+gulp.task('default', function() {
+  gulp.src('src/*.js')
+    .pipe(uglify({
+    	preserveComments: "license"
+    }))
+    .pipe(rename({
+      extname: '.min.js'
+    }))
+    .pipe(gulp.dest('dist'));
+});
